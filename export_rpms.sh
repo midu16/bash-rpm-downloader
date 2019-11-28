50d5c4f72e3687cbb4ea99680cb304b34479fd6d
Mihai I <idumihai16@gmail.com>
2019-11-28 20:15:38 +0100

    added all the packets that required to be exported

--- export_rpms.sh
+++ export_rpms.sh
@@ -13,14 +13,25 @@ do
 	if [ "${file}" == "$WORKING_DIRECTORY/pachets_list" ]
 	then
 		countNumberofPackets=$(wc pachets_list | awk '{ print $2 }')
+		echo "================================================="
 		echo " A total number of ${countNumberofPackets} will be exported as rpm's and transfer to ansible-playbook /files/* "
 		if [ ! -d "$DIRECTORY" ]; then
 		#Creates the directory rpms/ obly if this one doesnt exists.
 			mkdir $DIRECTORY/
 		fi
+		echo "================================================="
+		echo "The following list of packages will pe exported: "
 		while read line; do echo $line; done < pachets_list
+		echo "================================================="
 		while read line; do sudo yumdownloader --resolve --destdir=$DIRECTORY/ $line; done < pachets_list
+		echo "================================================="
+		countNumberofRPMs=$(ls -l  $DIRECTORY/ | grep -v ^l | wc -l)
+		echo "The number of rpm's files exported is: $countNumberofRPMs"
+		echo "================================================="
 		break
 	fi
 done
 
+##
+# EOF
+##
